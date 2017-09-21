593a00deeb660c66c0467a75d7c80595f7a659ee
Yimin Zhou <yiminzhou@Yimins-MacBook-Pro.local>
2017-09-20 23:59:32 -0400

    add start and end check before Binary Search

--- Classical Binary Search.java
+++ Classical Binary Search.java
@@ -11,6 +11,12 @@ public class Solution {
         }
         int start = 0;
         int end = nums.length - 1;
+        if (nums[start] == target) {
+            return start;
+        }
+        if (nums[end] == target) {
+            return end;
+        }
         while ((start + 1) < end) {
             int mid = start + (end - start) / 2;
             if (nums[mid] == target) {
